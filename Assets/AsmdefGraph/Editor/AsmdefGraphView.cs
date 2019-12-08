b7be961b5f17d94c9bde1c7eadb8aa5850457543
nakano yosuke <naninuneno0411@gmail.com>
2019-12-09 00:09:19 +0900

    connect ports

--- Assets/AsmdefGraph/Editor/AsmdefGraphView.cs
+++ Assets/AsmdefGraph/Editor/AsmdefGraphView.cs
@@ -1,9 +1,17 @@
-﻿using UnityEditor.Experimental.GraphView;
+﻿using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
 
 namespace AsmdefGraph.Editor {
     public class AsmdefGraphView : GraphView {
         public AsmdefGraphView() : base() {
             AddElement(new AsmdefNode());
+            AddElement(new AsmdefNode());
+            this.AddManipulator(new SelectionDragger());
+        }
+
+        public override List<Port> GetCompatiblePorts(Port startAnchor, NodeAdapter nodeAdapter) {
+            return ports.ToList();
         }
     }
 }
--- Assets/AsmdefGraph/Editor/AsmdefNode.cs
+++ Assets/AsmdefGraph/Editor/AsmdefNode.cs
@@ -2,6 +2,14 @@
 
 namespace AsmdefGraph.Editor {
     public class AsmdefNode : Node {
+        public AsmdefNode() {
+            title = "AsmdefNode";
 
+            var inputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Input, Port.Capacity.Single, typeof(Port));
+            inputContainer.Add(inputPort);
+
+            var outputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(Port));
+            outputContainer.Add(outputPort);
+        }
     }
 }
